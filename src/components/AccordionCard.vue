d7dbeb2f12b81fa586fad3ef1819a359446f8d3c
Antoine Cheminat <ac.dev@hotmail.com>
2024-11-09 10:45:29 +0100

    Create desktop styles (#2)

--- src/components/AccordionCard.vue
+++ src/components/AccordionCard.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import accordionItems from '@/assets/json/accordionItems.json'
-import StarIcon from '@/components/icons/StarIcon.vue'
-import PlusIcon from '@/components/icons/PlusIcon.vue'
-import MinusIcon from './icons/MinusIcon.vue'
+import { ref, type Ref } from 'vue';
+import accordionItems from '@/assets/json/accordionItems.json';
+import StarIcon from '@/components/icons/StarIcon.vue';
+import PlusIcon from '@/components/icons/PlusIcon.vue';
+import MinusIcon from './icons/MinusIcon.vue';
 
-const activeItemIndex = ref(0)
+const activeItemIndex: Ref<number | null> = ref(accordionItems[0].id);
 const setActiveIndex = (index: number) => {
-  activeItemIndex.value = index
-}
+  activeItemIndex.value = index === activeItemIndex.value ? null : index;
+};
 </script>
 
 <template>
@@ -17,15 +17,11 @@ const setActiveIndex = (index: number) => {
       <StarIcon class="accordion-header__icon" />
       <h1 class="accordion-header__title">FAQs</h1>
     </div>
-    <div
-      class="accordion-item"
-      v-for="(item, index) in accordionItems"
-      :key="item.id"
-    >
-      <button class="accordion-item__button" @click="setActiveIndex(index)">
+    <div class="accordion-item" v-for="item in accordionItems" :key="item.id">
+      <button class="accordion-item__button" @click="setActiveIndex(item.id)">
         <h2 class="accordion-item__title">{{ item.title }}</h2>
         <MinusIcon
-          v-if="activeItemIndex === index"
+          v-if="activeItemIndex === item.id"
           class="accordion-item__icon-minus"
         />
         <PlusIcon v-else class="accordion-item__icon-plus" />
@@ -33,7 +29,7 @@ const setActiveIndex = (index: number) => {
       <p
         :class="[
           'accordion-item__content',
-          { show: activeItemIndex === index },
+          { show: activeItemIndex === item.id },
         ]"
       >
         <span>{{ item.content }}</span>
@@ -46,7 +42,7 @@ const setActiveIndex = (index: number) => {
 .accordion {
   width: min(100%, 37.5rem);
   margin-inline: auto;
-  padding: 1.5rem;
+  padding: clamp(1.5rem, -2.5rem + 13.3333vw, 2.5rem);
   border-radius: 0.5rem;
   background-color: hsl(var(--clr-surface));
   box-shadow: var(--box-shadow);
@@ -59,12 +55,12 @@ const setActiveIndex = (index: number) => {
   margin-block-end: 1.5rem;
 
   &__icon {
-    width: 21px;
+    width: clamp(1.5rem, -1rem + 8.3333vw, 2.125rem);
     color: hsl(var(--clr-primary));
   }
 
   &__title {
-    font-size: var(--fs-lg);
+    font-size: var(--fs-title);
     font-weight: var(--fw-bold);
     line-height: var(--lh-title);
     color: hsl(var(--clr-text-200));
@@ -88,6 +84,7 @@ const setActiveIndex = (index: number) => {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    gap: 1.5rem;
 
     &:focus-visible {
       outline: 2px dotted hsl(var(--clr-primary));
@@ -97,11 +94,10 @@ const setActiveIndex = (index: number) => {
 
   &__title {
     text-align: start;
-    font-size: var(--fs-base);
+    font-size: var(--fs-item-title);
     font-weight: var(--fw-medium);
     line-height: var(--lh-title);
     color: hsl(var(--clr-text-200));
-    max-width: 22ch;
   }
 
   &__icon-plus,
@@ -124,7 +120,7 @@ const setActiveIndex = (index: number) => {
     transition: 250ms ease-in-out;
 
     & > span {
-      font-size: var(--fs-sm);
+      font-size: var(--fs-item-content);
       display: block;
       overflow: hidden;
     }
